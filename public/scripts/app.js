3538c6127ee12e20189d488c7ae146336eeae687
aaronhorowitz3 <aaron.horowitz3@gmail.com>
2018-05-29 10:22:58 -0700

    Delete app.js

--- public/scripts/app.js
+++
@@ -1,9 +0,0 @@
-let url = https://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V&json=y
-
-axios.get(url)
-.then(function(response) {
-  console.log(response.root.stations.station)
-});
-.catch(function(err){
-  console.log(err)
-});
