eb412a2962d9366a8320f91c3feb8642626962da
aaronhorowitz3 <aaron.horowitz3@gmail.com>
2018-05-31 16:06:31 -0700

    added mongodb uri

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/allways_test');
+mongoose.connect(process.env.MONGODB_URI || localhost:3000);
 
 let Station = require('./station');
 
